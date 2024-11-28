5635ff6b650599fc177cd5881595fe0ad17c7dbd
abibhatta100@gmail.com <abibhatta100@gmail.com>
2024-11-28 10:01:21 +0545

    initial commit

--- market.py
+++ market.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World from Flask to Bihan'
+    return 'Hello, World from Flask to Abi'
 
 @app.route('/about/<username>')
 def about_page(username):
